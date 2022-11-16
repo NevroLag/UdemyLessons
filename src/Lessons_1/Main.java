e7306315ddc519d55831055903482a4a5eca1bee
”NevroLag” <aleksandrkonshin03@gmail.com>
2022-11-16 23:23:53 +0300

    Раздел №2 дз 1, без материалов урока.

--- src/Lessons_1/Main.java
+++ src/Lessons_1/Main.java
@@ -1,2 +1,13 @@
-package Lessons_1;public class Main {
+package Lessons_1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("подготовка к лекциям Юдеми");
+        System.out.println("Роман");
+        System.out.println("Михаил");
+        System.out.println("Ирина");
+        System.out.println("Андрей");
+        System.out.println("Алексей");
+        System.out.println("Ольга");
+    }
 }
