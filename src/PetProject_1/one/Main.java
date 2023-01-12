8e3b7d3fb4f2a4e1b2bcc934eea6cad7002b5c94
”NevroLag” <aleksandrkonshin03@gmail.com>
2023-01-12 17:55:00 +0300

    Pet-P

--- src/PetProject_1/one/Main.java
+++ src/PetProject_1/one/Main.java
@@ -2,11 +2,23 @@ package PetProject_1.one;
 
 public class Main {
     public static void main(String[] args) {
+/*
+        Чтобы использовать этот класс, вы можете создать новый экземпляр класса VirtualPet, например:
+*/
         VirtualPet pet = new VirtualPet("Fluffy");
+/*
+        Затем вы можете взаимодействовать с домашним животным, вызывая его методы, например:
+*/
         pet.feed();
         pet.water();
         pet.sleep();
         pet.play();
+        pet.tick();
         System.out.println();
     }
 }
+
+/*
+Вы можете добавить в класс дополнительную функциональность, например, добавить графический интерфейс,
+отобразить статус питомца, добавить параметры меню и т. д.
+*/
